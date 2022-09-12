64a34e35cc9e05cd83760d8f7ffe1fa0b42feef6
amberm350 <112363236+amberm350@users.noreply.github.com>
2022-09-11 22:44:26 -0400

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -3,6 +3,11 @@ import urllib.request
 import argparse
 import logging
 
+LOG_FILENAME = 'error.log'
+logging.basicConfig(filename=LOG_FILENAME,
+    level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+
 def downloadData(url):
     with urllib.request.urlopen(url) as response:
         url_data = response.read().decode('utf-8')
@@ -42,6 +47,7 @@ def main(url):
     data = downloadData(url)
     personData = processData(data)
     print ('Input an ID')
+    
     id = 33
     if id <= 0:
         quit()
@@ -50,9 +56,10 @@ def main(url):
     
 if __name__ == "__main__":
     url = 'https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'
-
+    idnum= int(input('ID'))
     my_parser = argparse.ArgumentParser(description='Assignment2 Parser')
     my_parser.add_argument('--url', type=str, required =True, help='The URL we want to download')
     args = my_parser.parse_args()
     url = args.url
     main(url)
+    main(idnum)
